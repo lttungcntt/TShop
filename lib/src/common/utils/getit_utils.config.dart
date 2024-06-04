dfa1db2e6eb7852ad9c2282109c50dc8123705b0
Tyler <tung.le@legato.co>
2024-06-04 21:04:16 +0700

    feat: add git ignore

--- lib/src/common/utils/getit_utils.config.dart
+++ lib/src/common/utils/getit_utils.config.dart
@@ -8,7 +8,9 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:dio/dio.dart' as _i17;
+import 'package:dio/dio.dart' as _i25;
+import 'package:get_it/get_it.dart' as _i1;
+import 'package:injectable/injectable.dart' as _i2;
 import 'package:t_shop/src/common/theme/color_theme/default_dart_color_theme.dart'
     as _i5;
 import 'package:t_shop/src/common/theme/color_theme/default_light_color_theme.dart'
@@ -19,52 +21,50 @@ import 'package:t_shop/src/common/utils/logger.dart' as _i31;
 import 'package:t_shop/src/core/application/cubits/auth/auth_cubit.dart'
     as _i30;
 import 'package:t_shop/src/core/application/cubits/lang/lang_cubit.dart'
-    as _i25;
+    as _i20;
 import 'package:t_shop/src/core/application/cubits/theme/theme_cubit.dart'
-    as _i24;
+    as _i26;
 import 'package:t_shop/src/core/domain/interfaces/lang_repository_interface.dart'
-    as _i15;
+    as _i8;
 import 'package:t_shop/src/core/domain/interfaces/theme_repository_interface.dart'
-    as _i19;
+    as _i14;
 import 'package:t_shop/src/core/infrastructure/datasources/remote/api/api_client.dart'
     as _i32;
 import 'package:t_shop/src/core/infrastructure/datasources/remote/api/services/auth/auth_client.dart'
     as _i27;
 import 'package:t_shop/src/core/infrastructure/repositories/lang_repository.dart'
-    as _i16;
+    as _i9;
 import 'package:t_shop/src/core/infrastructure/repositories/theme_repository.dart'
-    as _i20;
+    as _i15;
 import 'package:t_shop/src/modules/app/app_router.dart' as _i7;
 import 'package:t_shop/src/modules/auth/domain/interfaces/auth_repository_interface.dart'
     as _i28;
 import 'package:t_shop/src/modules/auth/infrastructure/repositories/auth_repository.dart'
     as _i29;
 import 'package:t_shop/src/modules/dashboard/application/cubit/dashboard_cubit.dart'
-    as _i18;
-import 'package:t_shop/src/modules/dashboard/domain/interfaces/dashboard_interface.dart'
-    as _i8;
-import 'package:t_shop/src/modules/dashboard/infrastructure/repositories/dashboard_mockup_repository.dart'
-    as _i9;
-import 'package:t_shop/src/modules/dashboard/infrastructure/repositories/dashboard_repository.dart'
-    as _i21;
-import 'package:t_shop/src/modules/home/application/cubit/home_cubit.dart'
-    as _i26;
-import 'package:t_shop/src/modules/home/domain/interfaces/home_interface.dart'
-    as _i12;
-import 'package:t_shop/src/modules/home/infrastructure/repositories/home_mockup_repository.dart'
-    as _i13;
-import 'package:t_shop/src/modules/home/infrastructure/repositories/home_repository.dart'
     as _i22;
+import 'package:t_shop/src/modules/dashboard/domain/interfaces/dashboard_interface.dart'
+    as _i12;
+import 'package:t_shop/src/modules/dashboard/infrastructure/repositories/dashboard_mockup_repository.dart'
+    as _i13;
+import 'package:t_shop/src/modules/dashboard/infrastructure/repositories/dashboard_repository.dart'
+    as _i24;
+import 'package:t_shop/src/modules/home/application/cubit/home_cubit.dart'
+    as _i21;
+import 'package:t_shop/src/modules/home/domain/interfaces/home_interface.dart'
+    as _i16;
+import 'package:t_shop/src/modules/home/infrastructure/repositories/home_mockup_repository.dart'
+    as _i17;
+import 'package:t_shop/src/modules/home/infrastructure/repositories/home_repository.dart'
+    as _i19;
 import 'package:t_shop/src/modules/home_detail/application/cubit/home_detail_cubit.dart'
-    as _i14;
+    as _i23;
 import 'package:t_shop/src/modules/home_detail/domain/interfaces/home_detail_interface.dart'
     as _i10;
 import 'package:t_shop/src/modules/home_detail/infrastructure/repositories/home_detail_mockup_repository.dart'
     as _i11;
 import 'package:t_shop/src/modules/home_detail/infrastructure/repositories/home_detail_repository.dart'
-    as _i23;
-import 'package:get_it/get_it.dart' as _i1;
-import 'package:injectable/injectable.dart' as _i2;
+    as _i18;
 import 'package:talker_flutter/talker_flutter.dart' as _i3;
 
 const String _ALPHA = 'ALPHA';
@@ -92,53 +92,26 @@ extension GetItInjectableX on _i1.GetIt {
     gh.singleton<_i6.DefaultLightColorTheme>(
         () => _i6.DefaultLightColorTheme());
     gh.singleton<_i7.AppRouter>(() => _i7.AppRouter());
-    gh.lazySingleton<_i8.IDashboardRepository>(
-      () => _i9.DashboardMockupRepository(),
-      registerFor: {_ALPHA},
-    );
+    gh.lazySingleton<_i8.ILangRepository>(() => _i9.LangRepository());
     gh.lazySingleton<_i10.IHomeDetailRepository>(
       () => _i11.HomeDetailMockupRepository(),
       registerFor: {_ALPHA},
     );
-    gh.lazySingleton<_i12.IHomeRepository>(
-      () => _i13.HomeMockupRepository(),
+    gh.lazySingleton<_i12.IDashboardRepository>(
+      () => _i13.DashboardMockupRepository(),
       registerFor: {_ALPHA},
     );
-    gh.factory<_i14.HomeDetailCubit>(
-        () => _i14.HomeDetailCubit(gh<_i10.IHomeDetailRepository>()));
     gh.factory<String>(
       () => apiModule.baseUrl,
       instanceName: 'baseUrl',
     );
-    gh.lazySingleton<_i15.ILangRepository>(() => _i16.LangRepository());
-    gh.singleton<_i17.Dio>(() => apiModule.dio(
-          gh<String>(instanceName: 'baseUrl'),
-          gh<_i15.ILangRepository>(),
-          gh<_i3.Talker>(),
-        ));
-    gh.singleton<_i18.DashboardCubit>(
-        () => _i18.DashboardCubit(gh<_i8.IDashboardRepository>()));
-    gh.lazySingleton<_i19.IThemeRepository>(() => _i20.ThemeRepository());
-    gh.lazySingleton<_i8.IDashboardRepository>(
-      () => _i21.DashboardRepository(),
-      registerFor: {
-        _DEV,
-        _PRG,
-        _UAT,
-        _PRD,
-      },
-    );
-    gh.lazySingleton<_i12.IHomeRepository>(
-      () => _i22.HomeRepository(),
-      registerFor: {
-        _DEV,
-        _PRG,
-        _UAT,
-        _PRD,
-      },
+    gh.lazySingleton<_i14.IThemeRepository>(() => _i15.ThemeRepository());
+    gh.lazySingleton<_i16.IHomeRepository>(
+      () => _i17.HomeMockupRepository(),
+      registerFor: {_ALPHA},
     );
     gh.lazySingleton<_i10.IHomeDetailRepository>(
-      () => _i23.HomeDetailRepository(),
+      () => _i18.HomeDetailRepository(),
       registerFor: {
         _DEV,
         _PRG,
@@ -146,13 +119,40 @@ extension GetItInjectableX on _i1.GetIt {
         _PRD,
       },
     );
-    gh.singleton<_i24.ThemeCubit>(
-        () => _i24.ThemeCubit(gh<_i19.IThemeRepository>()));
-    gh.singleton<_i25.LangCubit>(
-        () => _i25.LangCubit(gh<_i15.ILangRepository>()));
-    gh.factory<_i26.HomeCubit>(
-        () => _i26.HomeCubit(gh<_i12.IHomeRepository>()));
-    gh.factory<_i27.AuthClient>(() => _i27.AuthClient(gh<_i17.Dio>()));
+    gh.lazySingleton<_i16.IHomeRepository>(
+      () => _i19.HomeRepository(),
+      registerFor: {
+        _DEV,
+        _PRG,
+        _UAT,
+        _PRD,
+      },
+    );
+    gh.singleton<_i20.LangCubit>(
+        () => _i20.LangCubit(gh<_i8.ILangRepository>()));
+    gh.factory<_i21.HomeCubit>(
+        () => _i21.HomeCubit(gh<_i16.IHomeRepository>()));
+    gh.singleton<_i22.DashboardCubit>(
+        () => _i22.DashboardCubit(gh<_i12.IDashboardRepository>()));
+    gh.factory<_i23.HomeDetailCubit>(
+        () => _i23.HomeDetailCubit(gh<_i10.IHomeDetailRepository>()));
+    gh.lazySingleton<_i12.IDashboardRepository>(
+      () => _i24.DashboardRepository(),
+      registerFor: {
+        _DEV,
+        _PRG,
+        _UAT,
+        _PRD,
+      },
+    );
+    gh.singleton<_i25.Dio>(() => apiModule.dio(
+          gh<String>(instanceName: 'baseUrl'),
+          gh<_i8.ILangRepository>(),
+          gh<_i3.Talker>(),
+        ));
+    gh.singleton<_i26.ThemeCubit>(
+        () => _i26.ThemeCubit(gh<_i14.IThemeRepository>()));
+    gh.factory<_i27.AuthClient>(() => _i27.AuthClient(gh<_i25.Dio>()));
     gh.lazySingleton<_i28.IAuthRepository>(
         () => _i29.AuthRepository(gh<_i27.AuthClient>()));
     gh.singleton<_i30.AuthCubit>(
